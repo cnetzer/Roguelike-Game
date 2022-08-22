8ab5bf85b709be241c00173aff86e33b1e06214f
cnetzer <2021gmenetchr@gmail.com>
2022-08-22 17:08:27 +0200

    --- INV UPDATE ---

--- Roguelike Game/Assets/Scripts/Inventory/GroundItem.cs
+++ Roguelike Game/Assets/Scripts/Inventory/GroundItem.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class GroundItem : MonoBehaviour
+public class GroundItem : MonoBehaviour, ISerializationCallbackReceiver
 {
     public InventoryItemData item;
+    public void OnBeforeSerialize()
+    {
+        //GetComponentInChildren<SpriteRenderer>().sprite = item.uiDisplay;
+    }
+
+    public void OnAfterDeserialize()
+    {
+        
+    }
 }
